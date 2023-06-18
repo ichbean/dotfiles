3e030fd630e9515748668d29af99e0577bf72f0d
ichbean <ichbean@gmail.com>
2023-06-18 23:22:27 +0300

    Add lua, py, rust servers

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -79,7 +79,11 @@ return {
                 lsp.default_keymaps({buffer = bufnr})
             end)
 
-            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+            lsp.ensure_installed({
+                'lua_ls',
+                'pyright',
+                'rust_analyzer'
+            })
 
             lsp.setup()
         end

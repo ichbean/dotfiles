e6c44e11bb8aa2b2ac1b3bc92674bece9067f8e2
ichbean <ichbean@gmail.com>
2023-07-11 00:08:09 +0300

    Load plugins later

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
 require("options")
 require("autocmds")
 require("bindings")
+require("lazy").setup("plugins")

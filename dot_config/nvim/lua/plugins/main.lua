939b249f4112fdcf8f401a59bd396f8573acb260
ichbean <ichbean@gmail.com>
2023-09-23 17:42:56 +0300

    Fix semantic highlight

--- dot_config/nvim/lua/plugins/main.lua
+++ dot_config/nvim/lua/plugins/main.lua
@@ -7,6 +7,25 @@ return {
             vim.g.nord_italic = false
             vim.g.nord_bold = false
             vim.cmd([[colorscheme nord]])
+            local links = {
+                ['@lsp.type.namespace'] = '@namespace',
+                ['@lsp.type.type'] = '@type',
+                ['@lsp.type.class'] = '@type',
+                ['@lsp.type.enum'] = '@type',
+                ['@lsp.type.interface'] = '@type',
+                ['@lsp.type.struct'] = '@structure',
+                ['@lsp.type.parameter'] = '@parameter',
+                ['@lsp.type.variable'] = '@variable',
+                ['@lsp.type.property'] = '@property',
+                ['@lsp.type.enumMember'] = '@constant',
+                ['@lsp.type.function'] = '@function',
+                ['@lsp.type.method'] = '@method',
+                ['@lsp.type.macro'] = '@macro',
+                ['@lsp.type.decorator'] = '@function',
+            }
+            for newgroup, oldgroup in pairs(links) do
+                vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+            end
         end
     },
 
@@ -19,14 +38,6 @@ return {
                 component_separators = "|",
                 section_separators = ""
             }
-        }
-    },
-
-    {
-        "echasnovski/mini.align",
-        version = "false",
-        opts = {
-            start = 'ga'
         }
     },
 

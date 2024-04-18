197d63ab327214652d3359b0e8ff7c65d9b10a02
ichbean <ichbean@gmail.com>
2024-04-19 00:23:58 +0300

    Add binding descriptions

--- dot_config/nvim/lua/bindings.lua
+++ dot_config/nvim/lua/bindings.lua
@@ -1,18 +1,14 @@
 vim.g.mapleader = ' '
-vim.g.localmapleader = ' '
 
-vim.keymap.set('n', '<CR>', ':') -- Command with Enter
-vim.keymap.set('n', '<BS>', 'ciw') -- Delete the word under the cursor
-vim.keymap.set({ 'i', 'v', 'n', 's' }, '<C-s>', '<CMD>w<cr><ESC>') -- Save with Ctrl-s
-vim.keymap.set('n', '<ESC>', '<CMD>noh<CR>', {silent = true}) -- Clear search with ESC
--- Move between splits
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
--- Replace word occurences
-vim.keymap.set('n', 'c*', '*Ncgn') -- Forwards
-vim.keymap.set('n', 'c#', '#NcgN') -- Backwards
--- Cycle through buffers
-vim.keymap.set('n', '<TAB>', '<CMD>bn<CR>')
-vim.keymap.set('n', '<S-TAB>', '<CMD>bp<CR>')
+vim.keymap.set('n', '<CR>', ':', {desc = 'Enter command'})
+vim.keymap.set('n', '<BS>', 'ciw', {desc='Change word'})
+vim.keymap.set({ 'i', 'v', 'n', 's' }, '<C-s>', '<CMD>w<CR><ESC>', {desc = 'Save file'})
+vim.keymap.set('n', '<ESC>', '<CMD>noh<CR>', {silent = true, desc = 'Clear search highlight'})
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {desc = 'Move to bottom split'})
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', {desc = 'Move to top split'})
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {desc = 'Move to right split'})
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', {desc = 'Move to left split'})
+vim.keymap.set('n', 'c*', '*Ncgn', {desc = 'Replace word forwards'})
+vim.keymap.set('n', 'c#', '#NcgN', {desc = 'Replace word backwards'})
+vim.keymap.set('n', '<TAB>', '<CMD>bn<CR>', {desc = 'Next buffer'})
+vim.keymap.set('n', '<S-TAB>', '<CMD>bp<CR>', {desc = 'Previous buffer'})
